aacf9b85e14286904e3446552e87548c02b0d5c6
Marcin Nowicki <pr0d1r2@gmail.com>
2013-12-22 14:32:44 +0100

    Change namespace for convenience.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ SimpleCov.minimum_coverage 100
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'ruby_osx_app'
+require 'osx_app'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
